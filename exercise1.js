e8fd8567501e4f0e7ca2f03123a03a334ede1dc1
Dean4132 <olalekanbamigboye732@gmail.com>
2025-12-28 20:18:33 +0100

    exercise 1 and 2

---
+++ exercise1.js
@@ -0,0 +1,42 @@
+
+//Question 1
+let firstName = "John";
+let lastName = "Doe";
+let Age = 25;
+
+console.log("My name is "  + firstName + " " + lastName + " and i am " + Age + " years old");
+
+//Question 2
+let num1 = 15;
+let num2 = 8;
+
+console.log(num1 + num2); //addition
+console.log(num1 - num2); //subtraction
+console.log(num1 * num2); //multiplication
+console.log(num1 / num2); //division
+
+//Question 3
+let temperature = 22;
+
+if ( temperature < 20) {
+    console.log("it's cold")
+}
+
+
+else{
+    console.log("it's warm")
+};
+
+//Question 4
+let price = 50;
+let discount = 10;
+
+const finalPrice = price - discount;
+console.log(finalPrice);
+
+//Question 5
+let number = 12;
+
+console.log(number == 0)
+console.log(number > 0)
+console.log(number < 0)
---
+++ exercise2.js
@@ -0,0 +1,46 @@
+//Question 6
+let score = 75;
+
+if (score >= 50){
+    console.log("pass")
+}
+else{
+    console.log("failed")
+};
+
+//Question 7
+let a = 12;
+let b = 15;
+
+console.log(a > b );
+
+//question 8
+let Age = 16;
+
+if(Age >= 18) {
+    console.log("Can Vote")
+}
+else{
+    console.log("Cannot Vote")
+}
+
+//Question 9
+let isWeekend = true;
+
+if (isWeekend) {
+    console.log("No work today")
+}
+
+else {
+    console.log("Time to Work")
+};
+
+//Question 10
+let number = 24;
+
+if (number  % 2 == 0){
+    console.log("is even")
+}
+else{
+    console.log("is odd")
+}
